abd0799600d25dc99ab2742bc356bfd15d6926fd
Casey Deriso <caseyderiso@gmail.com>
2021-03-19 17:10:31 -0500

    created query and mutation resolvers in resolver.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,64 @@
+const { User } = require("../models");
+
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+      .select("-__v -password")
+    },
+  },
+
+  Mutation: {
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (partent, args) => {
+      const user = await User.findOne({ $or: [{ username: args.username }, {email: args.email}]})
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }     
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { user, args }) => {
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user.id },
+          { $addToSet: { savedBooks: args } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser
+      } catch (err) {
+        console.error(err);
+      };
+    },
+    deleteBook: async (parent, { user, args }) =>{
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: args.bookId } } },
+        { new: true}
+      )
+      if (!updatedUser) {
+        console.error("No user found")
+      }
+      return updatedUser
+    }
+  }
+};
+
+module.exports = resolvers;
